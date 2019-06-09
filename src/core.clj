67c1ecdf929b1a93713bc8a3a3c65902bc16ead8
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-06-09 19:28:06 -0400

    Experimenting with routing/request handling.

--- src/core.clj
+++ src/core.clj
@@ -4,5 +4,13 @@
             [io.pedestal.http.route :as route]))
             ; Handles routing for urls.
 
+(defn put-route [req]
+  {:status 200 :body "This was a put request"})
+
 (defn respond-hello [req]
-  {:status 200 :body "Hello World!"})
+  {:status 200 :body {:greeting "Hello World"
+                      :greeter "Pedestal"}})
+(def routes
+  (route/expand-routes
+   #{["/greet" :get respond-hello :route-name :greet]
+     ["/putrequest" :put put-route :route-name :put-route]}))
